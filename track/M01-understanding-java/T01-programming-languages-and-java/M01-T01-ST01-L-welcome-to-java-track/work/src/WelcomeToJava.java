35ea9e9b6b8fd1f1d8c8fe09d5f44f53d77e337e
manohariwadekar <manohariwadekar@gmail.com>
2026-09-01 17:59:25 +0530

    I have written my frist code

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,7 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
        //Write your code here
+       System.out.println("welcome to java");
+       System.out.println("I am in java Track");
     }
 }
